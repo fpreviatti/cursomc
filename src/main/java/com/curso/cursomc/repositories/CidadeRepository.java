15a6dc75c8218434f0272000f1e1925bb2cdc4b6
Fábio Previatti <fpreviatti@hotmail.com>
2020-08-20 20:52:27 -0300

    Estado e cidade

---
+++ src/main/java/com/curso/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.curso.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.cursomc.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+	
+	
+	
+}
---
+++ src/main/java/com/curso/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.curso.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
